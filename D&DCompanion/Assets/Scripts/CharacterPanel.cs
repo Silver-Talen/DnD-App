357a085ca206fc4edbf46eda2456d92e8c3a5aaf
Silver-Talen <33160173+Silver-Talen@users.noreply.github.com>
2020-03-02 12:58:27 -0700

    Die Selection UI

---
+++ D&DCompanion/Assets/Scripts/CharacterPanel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterPanel : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject CharInfoPanel = null;
+    [SerializeField]
+    public GameObject BaseStatsPanel = null;
+    [SerializeField]
+    public GameObject CombatInfoPanel = null;
+}

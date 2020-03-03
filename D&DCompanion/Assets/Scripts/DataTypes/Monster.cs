292039166860e851894aa7e3c645c61fd11a8fa1
Neko Anstle <nekoanstle@gmail.com>
2020-03-03 12:18:36 -0700

    Creating monster data class

---
+++ D&DCompanion/Assets/Scripts/DataTypes/Monster.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    string Index;
+    string Name;
+    string Size;
+    string Type;
+    string Subtype;
+    string Alignment;
+    int ArmorClass;
+    int HitPoints;
+    string HitDice;
+    List<string> Speed;
+    int Strength;
+    int Dexterity;
+    int Constitution;
+    int Intelligence;
+    int Wisdom;
+    int Charisma;
+    List<string> Proficiencies;
+}
